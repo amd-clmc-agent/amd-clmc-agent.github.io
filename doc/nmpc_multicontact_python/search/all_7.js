5cb0ab5a5182b50e88a25957e1f9efadf2209237
Vincent Berenz <vberenz@tuebingen.mpg.de>
2018-02-22 22:10:59 +0100

    Thu Feb 22 22:10:59 2018 on everest_Ubuntu_14_04_trusty

--- doc/nmpc_multicontact_python/search/all_7.js
+++ doc/nmpc_multicontact_python/search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['getstairswalkingcontact',['getStairsWalkingContact',['../classsrc_1_1nmpc__multicontact__python_1_1utils_1_1data_1_1Data.html#a19f82065385eb4e39f5149a99b8cb83c',1,'src::nmpc_multicontact_python::utils::data::Data']]],
   ['getstraightwalkingcontact',['getStraightWalkingContact',['../classsrc_1_1nmpc__multicontact__python_1_1utils_1_1data_1_1Data.html#ad8766a3c79a4edfdfb6ca5c2314e36bb',1,'src::nmpc_multicontact_python::utils::data::Data']]],
   ['graphplot',['GraphPlot',['../classsrc_1_1nmpc__multicontact__python_1_1utils_1_1plot_1_1GraphPlot.html',1,'src::nmpc_multicontact_python::utils::plot']]]
 ];
