6f17803ab64ba4920597c3a36ed63054d1ced088
Vincent Berenz <vberenz@tuebingen.mpg.de>
2018-02-26 17:41:27 +0100

    Mon Feb 26 17:41:27 2018 on everest_Ubuntu_14_04_trusty

--- doc/nmpc_multicontact_python/search/classes_10.js
+++ doc/nmpc_multicontact_python/search/classes_10.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['wholebodykineticenergymanipulabilityoptimization',['WholeBodyKineticEnergyManipulabilityOptimization',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimizb8819cdaf1d30b4071dabe718546fbe0.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]],
-  ['wholebodykineticmanipulabiltycost',['WholeBodyKineticManipulabiltyCost',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimizee8af6dec1ec6169464f491e6720f09a.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]],
   ['wholebodymanipulabilitycost',['WholeBodyManipulabilityCost',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimizfcb069670170ca722753a7d77762c0ce.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]],
   ['wholebodymanipulabilityoptimization',['WholeBodyManipulabilityOptimization',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimiza43c8fd31284b34d4bb6893f2dea7ce1.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]]
 ];
