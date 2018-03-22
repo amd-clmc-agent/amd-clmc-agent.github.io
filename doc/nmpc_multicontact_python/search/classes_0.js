128aaac8a800b631088a6e9e30491f7fe040c371
Vincent Berenz <vberenz@tuebingen.mpg.de>
2018-03-22 14:49:16 +0100

    Thu Mar 22 14:49:16 2018 on everest_Ubuntu_14_04_trusty

--- doc/nmpc_multicontact_python/search/classes_0.js
+++ doc/nmpc_multicontact_python/search/classes_0.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['analyticalinversekinematics',['AnalyticalInverseKinematics',['../classsrc_1_1nmpc__multicontact__python_1_1robots_1_1athena__inverse__kinematics_1_1AnalyticalInverseKinematics.html',1,'src::nmpc_multicontact_python::robots::athena_inverse_kinematics']]],
-  ['angularmomentumconstraints',['AngularMomentumConstraints',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact_1_1constraint__multicontact_1_1AngularMomentumConstraints.html',1,'src::nmpc_multicontact_python::multicontact::constraint_multicontact']]],
   ['angularmomentumconstraints',['AngularMomentumConstraints',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__manipulability_1_1constraint__multicontac9a56cf7047908cfb292357599be63c1.html',1,'src::nmpc_multicontact_python::multicontact_manipulability::constraint_multicontact']]],
+  ['angularmomentumconstraints',['AngularMomentumConstraints',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact_1_1constraint__multicontact_1_1AngularMomentumConstraints.html',1,'src::nmpc_multicontact_python::multicontact::constraint_multicontact']]],
+  ['angularmomentumconstraints',['AngularMomentumConstraints',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1constraint__multicontact_1_1AngularMomentumConstraints.html',1,'src::nmpc_multicontact_python::multicontact_momentum::constraint_multicontact']]],
   ['athenalimbsinversekinematics',['AthenaLimbsInverseKinematics',['../classsrc_1_1nmpc__multicontact__python_1_1robots_1_1athena_1_1AthenaLimbsInverseKinematics.html',1,'src::nmpc_multicontact_python::robots::athena']]],
   ['athenawrapper',['AthenaWrapper',['../classsrc_1_1nmpc__multicontact__python_1_1robots_1_1athena__wrapper_1_1AthenaWrapper.html',1,'src::nmpc_multicontact_python::robots::athena_wrapper']]]
 ];
