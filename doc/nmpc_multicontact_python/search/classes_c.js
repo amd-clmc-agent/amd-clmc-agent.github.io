03bb0b9bd8bfab6f98ccabce2fb89245e09d75e0
Vincent Berenz <vberenz@tuebingen.mpg.de>
2018-03-27 17:59:36 +0200

    Tue Mar 27 17:59:36 2018 on everest_Ubuntu_14_04_trusty

--- doc/nmpc_multicontact_python/search/classes_c.js
+++ doc/nmpc_multicontact_python/search/classes_c.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['quadraticcost',['QuadraticCost',['../classsrc_1_1nmpc__multicontact__python_1_1py__ipopt_1_1cost_1_1QuadraticCost.html',1,'src::nmpc_multicontact_python::py_ipopt::cost']]],
   ['quaternionconstraint',['QuaternionConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimization_1_1QuaternionConstraint.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]],
-  ['quaternionconstraint',['QuaternionConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__ik__optimization_1_1QuaternionConstraint.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_ik_optimization']]],
   ['quaternionconstraint',['QuaternionConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__optimization__end__effector_1_1QuaternionConstraint.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_optimization_end_effector']]],
+  ['quaternionconstraint',['QuaternionConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__ik__optimization_1_1QuaternionConstraint.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_ik_optimization']]],
   ['quaternioncost',['QuaternionCost',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimization_1_1QuaternionCost.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]]
 ];
--- doc/nmpc_multicontact_python/search/classes_e.js
+++ doc/nmpc_multicontact_python/search/classes_e.js
@@ -2,13 +2,12 @@ var searchData=
 [
   ['simpleconstraint',['SimpleConstraint',['../classtest_1_1solver_1_1test__solver__wrapper_1_1SimpleConstraint.html',1,'test::solver::test_solver_wrapper']]],
   ['simplecost',['SimpleCost',['../classtest_1_1solver_1_1test__solver__wrapper_1_1SimpleCost.html',1,'test::solver::test_solver_wrapper']]],
-  ['solvermulticontact',['SolverMulticontact',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__manipulability_1_1solver__multicontact_1_1SolverMulticontact.html',1,'src::nmpc_multicontact_python::multicontact_manipulability::solver_multicontact']]],
   ['solvermulticontact',['SolverMulticontact',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact_1_1solver__multicontact_1_1SolverMulticontact.html',1,'src::nmpc_multicontact_python::multicontact::solver_multicontact']]],
+  ['solvermulticontactmanipulability',['SolverMulticontactManipulability',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__manipulability_1_1solver__multicontact_1afc7e681561cfd13753514667679f2ab.html',1,'src::nmpc_multicontact_python::multicontact_manipulability::solver_multicontact']]],
   ['solvermulticontactmomentum',['SolverMulticontactMomentum',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1solver__multicontact_1_1SolverMulticontactMomentum.html',1,'src::nmpc_multicontact_python::multicontact_momentum::solver_multicontact']]],
   ['solverwrapper',['SolverWrapper',['../classsrc_1_1nmpc__multicontact__python_1_1py__ipopt_1_1solver__wrapper_1_1SolverWrapper.html',1,'src::nmpc_multicontact_python::py_ipopt::solver_wrapper']]],
-  ['stateintegration',['StateIntegration',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__manipulability_1_1integration_1_1StateIntegration.html',1,'src::nmpc_multicontact_python::multicontact_manipulability::integration']]],
-  ['stateintegration',['StateIntegration',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact_1_1integration_1_1StateIntegration.html',1,'src::nmpc_multicontact_python::multicontact::integration']]],
   ['stateintegration',['StateIntegration',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1integration_1_1StateIntegration.html',1,'src::nmpc_multicontact_python::multicontact_momentum::integration']]],
-  ['sumforcezcost',['SumForcezCost',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact_1_1cost__multicontact_1_1SumForcezCost.html',1,'src::nmpc_multicontact_python::multicontact::cost_multicontact']]],
-  ['sumforcezcost',['SumForcezCost',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1cost__multicontact_1_1SumForcezCost.html',1,'src::nmpc_multicontact_python::multicontact_momentum::cost_multicontact']]]
+  ['stateintegration',['StateIntegration',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact_1_1integration_1_1StateIntegration.html',1,'src::nmpc_multicontact_python::multicontact::integration']]],
+  ['sumforcezcost',['SumForcezCost',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1cost__multicontact_1_1SumForcezCost.html',1,'src::nmpc_multicontact_python::multicontact_momentum::cost_multicontact']]],
+  ['sumforcezcost',['SumForcezCost',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact_1_1cost__multicontact_1_1SumForcezCost.html',1,'src::nmpc_multicontact_python::multicontact::cost_multicontact']]]
 ];
