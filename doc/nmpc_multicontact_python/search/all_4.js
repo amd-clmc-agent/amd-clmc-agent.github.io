22a367fb36d51413d38c0d334c6bfaaf4f44dbfe
Vincent Berenz <vberenz@tuebingen.mpg.de>
2018-03-28 18:23:08 +0200

    Wed Mar 28 18:23:08 2018 on everest_Ubuntu_14_04_trusty

--- doc/nmpc_multicontact_python/search/all_4.js
+++ doc/nmpc_multicontact_python/search/all_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['data',['Data',['../classsrc_1_1nmpc__multicontact__python_1_1utils_1_1data_1_1Data.html',1,'src::nmpc_multicontact_python::utils::data']]],
+  ['datacollector',['DataCollector',['../classsrc_1_1nmpc__multicontact__python_1_1utils_1_1data__collector_1_1DataCollector.html',1,'src::nmpc_multicontact_python::utils::data_collector']]],
   ['display',['Display',['../classsrc_1_1nmpc__multicontact__python_1_1utils_1_1display_1_1Display.html',1,'src::nmpc_multicontact_python::utils::display']]],
   ['displayfloor',['displayfloor',['../classsrc_1_1nmpc__multicontact__python_1_1utils_1_1display_1_1Display.html#a210570be0db40c25ce574144106579c0',1,'src::nmpc_multicontact_python::utils::display::Display']]]
 ];
