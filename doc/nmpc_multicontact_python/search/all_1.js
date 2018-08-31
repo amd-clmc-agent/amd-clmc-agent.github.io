424161e0a0347745003481c6074f4e37043c9c69
Vincent Berenz <vberenz@tuebingen.mpg.de>
2018-08-31 18:01:26 +0200

    Fri Aug 31 18:01:26 2018 on everest_Ubuntu_14_04_trusty

--- doc/nmpc_multicontact_python/search/all_1.js
+++ doc/nmpc_multicontact_python/search/all_1.js
@@ -5,6 +5,7 @@ var searchData=
   ['angularmomentumcost',['AngularMomentumCost',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1cost__multicontact_1_1AngularMomentumCost.html',1,'src::nmpc_multicontact_python::multicontact_momentum::cost_multicontact']]],
   ['angularmomentumvariationconstraint',['AngularMomentumVariationConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1constraint__multicontact_1_18f493f257e036946c0738d4df0dda992.html',1,'src::nmpc_multicontact_python::multicontact_momentum::constraint_multicontact']]],
   ['angularmomentumvariationcost',['AngularMomentumVariationCost',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1cost__multicontact_1_1AngularMomentumVariationCost.html',1,'src::nmpc_multicontact_python::multicontact_momentum::cost_multicontact']]],
+  ['angularmomentumzeroconstraint',['AngularMomentumZeroConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1multicontact__momentum_1_1constraint__multicontact_1_1AngularMomentumZeroConstraint.html',1,'src::nmpc_multicontact_python::multicontact_momentum::constraint_multicontact']]],
   ['athenalimbsinversekinematics',['AthenaLimbsInverseKinematics',['../classsrc_1_1nmpc__multicontact__python_1_1robots_1_1athena_1_1AthenaLimbsInverseKinematics.html',1,'src::nmpc_multicontact_python::robots::athena']]],
   ['athenawrapper',['AthenaWrapper',['../classsrc_1_1nmpc__multicontact__python_1_1robots_1_1athena__wrapper_1_1AthenaWrapper.html',1,'src::nmpc_multicontact_python::robots::athena_wrapper']]]
 ];
--- doc/nmpc_multicontact_python/search/all_10.js
+++ doc/nmpc_multicontact_python/search/all_10.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['quadraticcost',['QuadraticCost',['../classsrc_1_1nmpc__multicontact__python_1_1py__ipopt_1_1cost_1_1QuadraticCost.html',1,'src::nmpc_multicontact_python::py_ipopt::cost']]],
   ['quaternion',['Quaternion',['../classsrc_1_1nmpc__multicontact__python_1_1mathematics_1_1quaternion_1_1Quaternion.html',1,'src::nmpc_multicontact_python::mathematics::quaternion']]],
+  ['quaternionconstraint',['QuaternionConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimization_1_1QuaternionConstraint.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]],
   ['quaternionconstraint',['QuaternionConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__ik__optimization_1_1QuaternionConstraint.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_ik_optimization']]],
   ['quaternionconstraint',['QuaternionConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__optimization__end__effector_1_1QuaternionConstraint.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_optimization_end_effector']]],
-  ['quaternionconstraint',['QuaternionConstraint',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimization_1_1QuaternionConstraint.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]],
   ['quaternioncost',['QuaternionCost',['../classsrc_1_1nmpc__multicontact__python_1_1manipulability_1_1manipulability__whole__body__optimization_1_1QuaternionCost.html',1,'src::nmpc_multicontact_python::manipulability::manipulability_whole_body_optimization']]],
   ['qw_5fpositive',['qw_positive',['../classsrc_1_1nmpc__multicontact__python_1_1mathematics_1_1quaternion_1_1Quaternion.html#a92a90cf102758e913712a112bfb3c4d4',1,'src::nmpc_multicontact_python::mathematics::quaternion::Quaternion']]]
 ];
